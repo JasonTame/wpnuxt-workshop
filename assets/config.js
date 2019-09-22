3999df8f6b67aac177f852e5700dbf66c0499930
Jason Tame <jason@pango.studio>
2019-09-22 21:29:28 +0200

    Tweaks to manifest and icon changes

--- assets/config.js
+++ assets/config.js
@@ -3,14 +3,14 @@
 const apiEndpoint = '2019.capetown.wordcamp.org' 
 
 export default {
-  appTitleShort: 'wcct-2019-nuxt-workshop',
-  appTitle: 'WCCT 2019 Nuxt Workshop',
-  appTitleShort: 'wcct-2019-nuxt-workshop',
+  appTitleShort: 'WCCT 2019 Nuxt PWA',
+  appTitle: 'WCCT 2019 Nuxt Workshop App',
   appDescription: 'App for the Nuxt workshop at WCCT 2019',
   appThemeColor: '#ffffff',
-  appBgColor: '#4a4a4a ',
+  appBgColor: '#252021',
   appIcon: 'assets/icon.png',
-  // these are the rest api endpoints and your wordpress url 
+
+  // WP Rest Api endpoints
   client: `https://${apiEndpoint}`, 
   wpDomain: `https://${apiEndpoint}/wp-json`,
   api: {
