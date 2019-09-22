4fa05843b8c92337035c31f952847b215ba57909
Jason Tame <jason@pango.studio>
2019-09-22 17:28:21 +0200

    Wrap img change in media query

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,76 +19,77 @@ export default {
 
 <style>
 .quicksand {
-    font-family: 'Nunito', sans-serif;
+  font-family: "Nunito", sans-serif;
 }
 
 ::selection {
-    background: #E9D8FD;
-color:#202684;
+  background: #e9d8fd;
+  color: #202684;
 
-    /* WebKit/Blink Browsers */
+  /* WebKit/Blink Browsers */
 }
 
 ::-moz-selection {
-    background: #E9D8FD;
-color:#202684;
-    /* Gecko Browsers */
+  background: #e9d8fd;
+  color: #202684;
+  /* Gecko Browsers */
 }
 
 a:not(.nav) {
-    font-weight: bold;
-    text-decoration: none;
-    padding: 2px;
-    background: linear-gradient(to right, #5A67D8, #5A67D8);
-    background-repeat: repeat-x;
-    background-size: 100% 2px;
-    background-position: 0 95%;
-    -webkit-transition: all 150ms ease-in-out;
-    -moz-transition: all 150ms ease-in-out;
-    -ms-transition: all 150ms ease-in-out;
-    -o-transition: all 150ms ease-in-out;
-    transition: all 150ms ease-in-out;
+  font-weight: bold;
+  text-decoration: none;
+  padding: 2px;
+  background: linear-gradient(to right, #5a67d8, #5a67d8);
+  background-repeat: repeat-x;
+  background-size: 100% 2px;
+  background-position: 0 95%;
+  -webkit-transition: all 150ms ease-in-out;
+  -moz-transition: all 150ms ease-in-out;
+  -ms-transition: all 150ms ease-in-out;
+  -o-transition: all 150ms ease-in-out;
+  transition: all 150ms ease-in-out;
 }
 
 a:hover {
-    color: #B794F4;
-    font-weight: bold;
-    text-decoration: none;
-    padding-bottom: 2px;
-    background: linear-gradient(to right, #9F7AEA, #E9D8FD);
-    background-repeat: repeat-x;
-    background-size: 100% 2px;
-    background-position: 50% 95%;
-    -webkit-transition: color 150ms ease-in-out;
-    -moz-transition: color 150ms ease-in-out;
-    -ms-transition: color 150ms ease-in-out;
-    -o-transition: color 150ms ease-in-out;
-    transition: color 150ms ease-in-out;
+  color: #b794f4;
+  font-weight: bold;
+  text-decoration: none;
+  padding-bottom: 2px;
+  background: linear-gradient(to right, #9f7aea, #e9d8fd);
+  background-repeat: repeat-x;
+  background-size: 100% 2px;
+  background-position: 50% 95%;
+  -webkit-transition: color 150ms ease-in-out;
+  -moz-transition: color 150ms ease-in-out;
+  -ms-transition: color 150ms ease-in-out;
+  -o-transition: color 150ms ease-in-out;
+  transition: color 150ms ease-in-out;
 }
 
 a:focus {
-    outline: none;
-    background: #E9D8FD;
+  outline: none;
+  background: #e9d8fd;
 }
 
 .post-content ul {
-    margin: 1.5rem 0;
+  margin: 1.5rem 0;
 }
 
 .post-content li {
-    margin: 1rem 0;
+  margin: 1rem 0;
 }
 
 .post-content form {
-    display: none;
+  display: none;
 }
 
 .post-content figure {
-    margin: 0;
+  margin: 0;
 }
 
-.post-content img {
+@media only screen and (min-width: 769px) {
+  .post-content img {
     max-width: 425px;
+  }
 }
-
 </style>
